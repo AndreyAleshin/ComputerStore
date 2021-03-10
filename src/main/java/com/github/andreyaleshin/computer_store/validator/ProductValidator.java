476e9b8fb6933475c998586d49fc5999a569633a
Andrey <andrey12.aa@gmail.com>
2021-03-10 17:44:59 +0200

    10.03.21 - Added ProductValidator and made changes to messages.properties

---
+++ src/main/java/com/github/andreyaleshin/computer_store/validator/ProductValidator.java
@@ -0,0 +1,34 @@
+package com.github.andreyaleshin.computer_store.validator;
+
+import com.github.andreyaleshin.computer_store.entity.Product;
+import org.springframework.stereotype.Component;
+import org.springframework.validation.Errors;
+import org.springframework.validation.ValidationUtils;
+import org.springframework.validation.Validator;
+
+@Component
+public class ProductValidator implements Validator {
+
+    public static final int MINIMUM_PRODUCT_NAME_LENGTH = 3;
+    public static final int MAXIMUM_PRODUCT_NAME_LENGTH = 24;
+
+    @Override
+    public boolean supports(Class<?> aClass) {
+        return Product.class.equals(aClass);
+    }
+
+    @Override
+    public void validate(Object target, Errors errors) {
+        Product product = (Product) target;
+
+        ValidationUtils.rejectIfEmptyOrWhitespace(errors, "name", "notEmpty.productForm.name");
+        ValidationUtils.rejectIfEmptyOrWhitespace(errors, "description", "notEmpty.productForm.description");
+        ValidationUtils.rejectIfEmptyOrWhitespace(errors, "price", "notEmpty.productForm.price");
+
+        if (product.getName().length() < MINIMUM_PRODUCT_NAME_LENGTH ||
+                product.getName().length() > MAXIMUM_PRODUCT_NAME_LENGTH) {
+            errors.rejectValue("name", "length.productForm.name");
+        }
+    }
+
+}
