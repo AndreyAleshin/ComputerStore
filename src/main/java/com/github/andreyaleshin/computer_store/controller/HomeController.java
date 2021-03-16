59aa00b67ba5e675fddf02d0f9c8732c70346861
Andrey <andrey12.aa@gmail.com>
2021-03-16 18:35:31 +0200

    16.03.21 - Rename MainController into HomeController; added LoginController; refactorings

--- src/main/java/com/github/andreyaleshin/computer_store/controller/HomeController.java
+++ src/main/java/com/github/andreyaleshin/computer_store/controller/HomeController.java
@@ -4,9 +4,6 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
-import java.security.Principal;
-
-// todo /user/** and /admin/**
 /*
 Контроллеры – это классы предназначенные для взаимодействия с клиентом (например web-страница)
 Специальный тип класса, применяемый в MVC приложениях.
@@ -14,38 +11,18 @@ import java.security.Principal;
 но с расширенными возможностями от Spring Framework.
  */
 @Controller
-public class MainController {
+public class HomeController {
 
     @GetMapping({"/", "/home"})
     public String showHome(Model model) {
         model.addAttribute("title", "Computer Store | Home");
-        model.addAttribute("message", "This is home page!");
+        model.addAttribute("message", "this is a home page!");
         return "/home";
-    }
-
-    // We don't define /login POST controller, it is provided by Spring Security
-    @GetMapping("/login")
-    public String login(Principal principal, Model model, String error) {
-        model.addAttribute("title", "Login page");
-
-        if (principal != null) {
-            return "redirect:/home"; // todo если уже залогинен перенапрлять на какую-нибудь страницу
-        }
-        if (error != null) {
-            model.addAttribute("errorMessage", "Invalid username and/or password");
-        }
-
-        return "/login";
     }
 
     @GetMapping("/about")
     public String showAbout() {
         return "/about";
-    }
-
-    @GetMapping("/error/403")
-    public String showAccessDenied() {
-        return "/error/403";
     }
 
 }
---
+++ src/main/java/com/github/andreyaleshin/computer_store/controller/LoginController.java
@@ -0,0 +1,28 @@
+package com.github.andreyaleshin.computer_store.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import java.security.Principal;
+
+@Controller
+public class LoginController {
+
+    // We don't define /login POST controller, it is provided by Spring Security
+    @GetMapping("/login")
+    public String login(Principal principal, Model model, String error) {
+        model.addAttribute("title", "Login page");
+
+        if (principal != null) {
+            // todo если уже залогинен перенапрлять на какую-нибудь страницу (скрипт: всплывающее окно сверху с сообщением)
+            return "redirect:/home";
+        }
+        if (error != null) {
+            model.addAttribute("loginErrorMessage", "Invalid username and/or password");
+        }
+
+        return "/login";
+    }
+
+}
